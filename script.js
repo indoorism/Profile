0742c08606585e8075e3550f97c9408957574f09
indoorism <anneok23@gmail.com>
2023-08-23 15:20:43 -0700

    added w3 slideshow

--- script.js
+++ script.js
@@ -1,39 +1,28 @@
-// script.js
-function setupSlideshow(container) {
-    const slideshowImages = container.querySelectorAll('.slideshow-image');
-    const prevButton = container.querySelector('.prev-button');
-    const nextButton = container.querySelector('.next-button');
+let slideIndex = 1;
+showSlides(slideIndex);
 
-    let currentSlide = 0;
-
-    function showSlide(slideIndex) {
-        if (slideIndex < 0) {
-            currentSlide = slideshowImages.length - 1;
-        } else if (slideIndex >= slideshowImages.length) {
-            currentSlide = 0;
-        }
-
-        slideshowImages.forEach(image => image.style.display = 'none');
-        slideshowImages[currentSlide].style.display = 'block';
-    }
-
-    function nextSlide() {
-        showSlide(currentSlide + 1);
-    }
-
-    function prevSlide() {
-        showSlide(currentSlide - 1);
-    }
-
-    // Add event listeners to the buttons
-    prevButton.addEventListener('click', prevSlide);
-    nextButton.addEventListener('click', nextSlide);
-
-    // Initial slide display
-    showSlide(currentSlide);
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
 
-// Find and set up all slideshow containers
-document.querySelectorAll('.slideshow-container').forEach(container => {
-    setupSlideshow(container);
-});
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
